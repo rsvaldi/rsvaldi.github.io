f02204896aa2a1caf95a75b04942675151cc5027
Roberto Svaldi <103134701+rsvaldi@users.noreply.github.com>
2024-11-12 15:32:11 +0100

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,9 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $messaggio = $_POST['messaggio'];
-    $data = $_POST['data'];  // Data selezionata
+    $nome_ospite = $_POST['nome_ospite'];
+    $email_ospite = $_POST['email_ospite'];
+    $nome_host = $_POST['nome_host'];
+    $email_host = $_POST['email_host'];
+    $data_arrivo = $_POST['data_arrivo'];  // Data arrivo selezionata
+    $data_partenza = $_POST['data_partenza'];  // Data partenza selezionata
     $hotel = $_POST['hotel'];  // Hotel selezionato
 
     // Per sapere quale email è stata precompilata, usa l'email dell'hotel (se selezionato)
@@ -15,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Se l'hotel è stato selezionato, usiamo l'email corrispondente
     if (isset($hotel) && array_key_exists($hotel, $emailHotel)) {
-        $email = $emailHotel[$hotel];  // Impostiamo l'email dell'hotel
+        $email_hotel = $emailHotel[$hotel];  // Impostiamo l'email dell'hotel
     }
 
     $to = "tuo@email.com";  // Cambia con il tuo indirizzo email
